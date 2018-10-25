1f096d2e02cb902710dba291293d9084eb24b147
MeditativeMind <zhengyike_unity@163.com>
2018-10-25 17:16:56 +0800

    栈 接口

---
+++ Assets/DataStructure/Stack/IStackDS.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Stack
+{
+    public interface IStackDS<T>
+    {
+        int Count { get; }
+        bool IsEmpty();
+        void Clear();
+        void Push(T item);
+        T Pop();
+        T Peek();
+    }
+}
