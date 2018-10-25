0650fe030b206e3f1429a9178de0f6cf871d7d57
MeditativeMind <zhengyike_unity@163.com>
2018-10-25 17:16:20 +0800

    顺序栈

---
+++ Assets/DataStructure/Stack/SequenceStack/SeqStack.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Stack
+{
+    //顺序栈 
+    public class SeqStack<T> : IStackDS<T>
+    {
+        private T[] data;
+        private int top;
+        public SeqStack(int size)
+        {
+            data = new T[size];
+            top = -1;
+        }
+        public SeqStack():this(50){ }
+
+        public int Count
+        {
+            get
+            {
+                return top + 1;
+            }
+        }
+
+        public void Clear()
+        {
+            top = -1;
+        }
+
+        public bool IsEmpty()
+        {
+            return Count == 0;
+        }
+
+        public T Peek()
+        {
+            if (top < 0)
+            {
+                Debug.LogError("The stack is empty!");
+                return default(T);
+            }
+            return data[top];
+        }
+
+        public T Pop()
+        {
+            if(top < 0 )
+            {
+                Debug.LogError("The stack is empty!");
+                return default(T);
+            }
+            T item = data[top];
+            top--;
+            return item;
+        }
+
+        public void Push(T item)
+        {
+            if(Count == data.Length)
+            {
+                Debug.LogError("The Stack is full!");
+                return;
+            }
+            data[top + 1] = item;
+            top++;
+        }
+       
+    }
+}
