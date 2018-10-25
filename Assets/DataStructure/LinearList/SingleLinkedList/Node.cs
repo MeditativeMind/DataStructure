1fa9d46438e5d9d0d646e224b5a9dab9942d035d
MeditativeMind <zhengyike_unity@163.com>
2018-10-25 17:15:19 +0800

    单链表节点类

---
+++ Assets/DataStructure/LinearList/SingleLinkedList/Node.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace SingleLinkedList
+{
+    public class Node<T>
+    {
+        private T data;//数据域 当前节点的数据
+        private Node<T> next; //引用域,下一个节点
+        public Node()
+        {
+            data = default(T);
+            next = null;
+        }
+        //构造器：数据域+引用域
+        public Node(Node<T> next, T data)
+        {
+            this.next = next;
+            this.data = data;
+        }
+        //构造器：引用域，头结点
+        public Node(Node<T> next)
+        {
+            this.next = next;
+        }
+        //构造器：数据域，尾结点
+        public Node(T data)
+        {
+            this.data = data;
+            next = null;
+        }
+        public Node<T> Next
+        {
+            get
+            {
+                return next;
+            }
+            set
+            {
+                next = value;
+            }
+        }
+        public T Data
+        {
+            get
+            {
+                return data;
+            }
+            set
+            {
+                data = value;
+            }
+        }
+    }
+}
