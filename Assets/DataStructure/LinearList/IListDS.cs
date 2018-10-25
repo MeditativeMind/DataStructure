bfa95df08b5b1ae4fb0e815643afd90541036046
MeditativeMind <zhengyike_unity@163.com>
2018-10-25 17:13:44 +0800

    顺序表接口

---
+++ Assets/DataStructure/LinearList/IListDS.cs
@@ -0,0 +1,18 @@
+﻿
+
+
+namespace LinearList
+{
+    public interface IListDS<T>
+    {
+        int Count { get; }
+        void Insert(T item, int index);
+        void Add(T item);
+        bool IsEmpty();
+        T GetElement(int index);
+        T Delete(int index);
+        T this[int index] { get; }
+        void Clear();
+        int LocateElement(T item);
+    }
+}
