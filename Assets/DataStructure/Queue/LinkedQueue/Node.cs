b110c3243663be0cb11299f1e3a2a94026b41864
MeditativeMind <zhengyike_unity@163.com>
2018-10-25 17:15:58 +0800

    队列节点类

---
+++ Assets/DataStructure/Queue/LinkedQueue/Node.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Queue
+{
+    public class Node<T>
+    {
+        private T data;//数据域 当前节点的数据
+        private Node<T> next;//引用域,下一个节点
+        public Node(T data)
+        {
+            this.data = data;
+        }
+        public T Data
+        {
+            get
+            {
+                return data;
+            }
+            set
+            {
+                data = value;
+            }
+        }
+        public Node<T> Next
+        {
+            get
+            {
+                return next;
+            }
+            set
+            {
+                next = value;
+            }
+
+        }
+
+    }
+}
