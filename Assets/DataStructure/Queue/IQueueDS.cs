2d5b2725dceb7384a1794b47673e883e1e1564b7
MeditativeMind <zhengyike_unity@163.com>
2018-10-25 17:15:34 +0800

    队列接口

---
+++ Assets/DataStructure/Queue/IQueueDS.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Queue
+{
+    public interface IQueueDS<T>
+    {
+        int Count { get; }
+        bool IsEmpty();
+        void Clear();
+        void Enqueue(T item);
+        T Dequeue();
+        T Peek();
+    }
+}
