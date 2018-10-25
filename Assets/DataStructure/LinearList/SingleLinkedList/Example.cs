e49dac953b140e0fac53b38a955d075846a19f9d
MeditativeMind <zhengyike_unity@163.com>
2018-10-25 17:14:45 +0800

    单链表例子

---
+++ Assets/DataStructure/LinearList/SingleLinkedList/Example.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace SingleLinkedList
+{
+    public class Example : MonoBehaviour
+    {
+        void Start()
+        {
+            LinkedList<string> list = new LinkedList<string>();
+            list.Add("1");
+            list.Add("2");
+            list.Insert("3",list.Count);
+            for (int i = 0; i < list.Count; i++)
+            {
+                print("第一次："+list[i]);
+            }
+            list.Delete(1);
+            for (int i = 0; i < list.Count; i++)
+            {
+                print("第二次：" + list[i]);
+            }
+        }
+    }
+}
